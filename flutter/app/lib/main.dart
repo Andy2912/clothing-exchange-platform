28177bc27c30710a2fee477af92bafd7646e8d08
Andy2912 <andyb2912@gmail.com>
2026-02-27 11:48:30 +0100

    Fix Android emulator API URL to 10.0.2.2

--- flutter/app/lib/main.dart
+++ flutter/app/lib/main.dart
@@ -37,7 +37,7 @@ class _HomePageState extends State<HomePage> {
 
   Future<void> fetchImage() async {
     try {
-      final res = await http.get(Uri.parse("http://localhost:8000/photo"));
+      final res = await http.get(Uri.parse("http://10.0.2.2:8000/photo"));
       if (res.statusCode != 200) {
         setState(() => error = "API error: ${res.statusCode}");
         return;
